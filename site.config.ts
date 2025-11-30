17dc7aa611757c4c07adde024f7b3002f357b686
paikalilly <paikalilly@gmail.com>
2025-12-01 00:50:50 +0200

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -26,7 +26,7 @@ export default siteConfig({
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
   defaultPageIcon: null,
-  defaultPageCover: '/star.png',
+  defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
   // whether or not to enable support for LQIP preview images (optional)
